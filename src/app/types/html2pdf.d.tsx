f22268bcc13d637939dc3031850b36a1a2413404
krish4221 <krishnakumar944385@gmail.com>
2024-12-17 11:29:42 +0530

    n

--- src/app/types/html2pdf.d.tsx
+++ src/app/types/html2pdf.d.tsx
@@ -1,12 +1,4 @@
 declare module 'html2pdf.js' {
-  interface Html2Pdf {
-    (): {
-      set: (options: Record<string, unknown>) => Html2Pdf;
-      from: (element: HTMLElement) => Html2Pdf;
-      save: () => void;
-    };
-  }
-
-  const html2pdf: Html2Pdf;
+  const html2pdf: any;
   export default html2pdf;
 }
